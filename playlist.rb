3e53b17646df57cdbfba48735eaee39e8b54d131
marthaberner <marthaberner.mb@gmail.com>
2015-03-12 09:46:44 -0700

    user can add a song to the playlist

--- playlist.rb
+++ playlist.rb
@@ -1,9 +1,16 @@
 class Playlist
+  attr_accessor :playlist
 
-  def initialize
+  def initialize(playlist = [])
+    @playlist = playlist
   end
 
   def empty?
+    playlist.length == 0
+  end
+
+  def add_song(song)
+    playlist << song
   end
 
 end
